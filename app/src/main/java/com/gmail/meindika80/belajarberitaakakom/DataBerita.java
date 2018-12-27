89cd9077d89c3241a83ccb5a0065b2b053c06cab
meindika <meindika80@gmail.com>
2018-12-27 11:49:25 +0700

    Testing Commit

---
+++ app/src/main/java/com/gmail/meindika80/belajarberitaakakom/DataBerita.java
@@ -0,0 +1,8 @@
+package com.gmail.meindika80.belajarberitaakakom;
+
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
